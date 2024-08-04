e03cf8b66c300a80c3ef335e7686bf62acc47eb3
orutanakaito <voltgt777@gmail.com>
2024-08-04 19:59:14 +0900

    while文で星と数字を表示させてみました。

--- Project1/Sample.cpp
+++ Project1/Sample.cpp
@@ -2,8 +2,13 @@
 #include <iostream>
 #include "lib\lib.cpp"
 int main() {
-
-    print("Hello");
-	print(2);
+	int i = 0;
+	
+	while (i < 5) {
+		std::cout << i << "\n";
+		i++;
+		print("š ");
+		print(0);
+	}
 	return 0;
 }
