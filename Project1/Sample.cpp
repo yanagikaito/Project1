7ec7c03449b3ec63e9a005305e2b034ef17d6baa
orutanakaito <voltgt777@gmail.com>
2024-08-04 19:12:33 +0900

    main関数のほうに#include "lib\lib.ｈ"だとビルド失敗したが,"lib\lib.cpp"だとビルド成功しました。

--- Project1/Sample.cpp
+++ Project1/Sample.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include "lib.h"
 int main() {
 	int num;
 	// キーボードから入力された情報がnumに記憶される。
@@ -7,5 +8,7 @@ int main() {
 	num = 2;
 	std::cout << num << std::endl;
 	printf("Hello\n");
+    print("Hello");
+	print(2);
 	return 0;
 }
