c3b3ee85232f0134bd75e0c7cb58facfd9c8bf35
orutanakaito <voltgt777@gmail.com>
2024-08-06 20:52:55 +0900

    #define DEBUGと#define MAX_VALUE (5)を定義し、#ifdefと#endif // DEBUGの処理を書いた

--- Project1/Sample.cpp
+++ Project1/Sample.cpp
@@ -1,13 +1,31 @@
 #include <stdio.h>
 #include <iostream>
 #include "lib\lib.cpp"
+#define MAX_VALUE (5)
+#define DEBUG
+
 int main() {
+
+	// デバッグ開始
+#ifdef DEBUG
+
 	int i = 0;
-	
-	while (i < 5) {
+
+	while (i < MAX_VALUE) {
 		std::cout << i << "\n";
 		i++;
-		print("★ ");
+		print("* ");
+		print(0);
+	}
+	return -1;
+
+	// デバッグ終了
+#endif // DEBUG
+	
+	while (i < MAX_VALUE) {
+		std::cout << i << "\n";
+		i++;
+		print("* ");
 		print(0);
 	}
 	return 0;
